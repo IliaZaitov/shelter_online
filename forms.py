553eb0d69311fee84f07269e7bc46490d9318256
Дунин Дмитрий <dunin_dmitrij@ws-r9.class-11.real-it>
2023-03-05 19:01:14 +0500

    Creating personages for new users

--- forms.py
+++ forms.py
@@ -1,17 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, Length,Email
+from wtforms.validators import DataRequired, Length, Email
+
 
 class RegForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
-    username = StringField("Username",validators=[DataRequired(),])
-    password = PasswordField("Password",validators=[DataRequired(), Length(min=5,max=100)])
-    password_repeat = PasswordField("Repeat the password", validators=[DataRequired(), Length(min=5,max=100)])
+    username = StringField("Username", validators=[DataRequired(), ])
+    personage_name = StringField("Personage name", validators=[DataRequired(), ])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=5, max=100)])
+    password_repeat = PasswordField("Repeat the password", validators=[DataRequired(), Length(min=5, max=100)])
     submit = SubmitField()
+
 
 class LoginForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), ])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=5,max=100)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=5, max=100)])
     submit = SubmitField()
     '''
     def validate_password_confirm(form, field):
--- models.py
+++ models.py
@@ -6,11 +6,13 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
+
 class PersonageModel(db.Model):
     __tablename__ = "personages"
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20), unique = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(20), unique=True)
     strength = db.Column(db.Integer, nullable=False)
     perception = db.Column(db.Integer, nullable=False)
     endurance = db.Column(db.Integer, nullable=False)
@@ -18,16 +20,16 @@ class PersonageModel(db.Model):
     intellect = db.Column(db.Integer, nullable=False)
     agility = db.Column(db.Integer, nullable=False)
     luck = db.Column(db.Integer, nullable=False)
-    max_hp = db.Column(db.Integer,nullable=False)
+    max_hp = db.Column(db.Integer, nullable=False)
     hp = db.Column(db.Integer, nullable=False)
     money = db.Column(db.Integer, nullable=False)
     experience = db.Column(db.Integer, nullable=False)
     stimpacks = db.Column(db.Integer, nullable=False)
-    avatar_path=db.Column(db.String(50))
-    state=db.Column(db.String(10))
+    avatar_path = db.Column(db.String(50))
+    state = db.Column(db.String(10))
 
     def __init__(self, name):
-        self.strength=random.randint(1,2)
+        self.strength = random.randint(1, 2)
         self.perception = random.randint(1, 2)
         self.endurance = random.randint(1, 2)
         self.charisma = random.randint(1, 2)
@@ -35,33 +37,38 @@ class PersonageModel(db.Model):
         self.agility = random.randint(1, 2)
         self.luck = random.randint(1, 2)
         self.name = name
-        self.max_hp = self.endurance*10+20
-        self.hp=self.max_hp
-        self.money = random.randint(10,20)
+        self.max_hp = self.endurance * 10 + 20
+        self.hp = self.max_hp
+        self.money = random.randint(10, 20)
         self.experience = random.randint(10, 20)
-        self.stimpacks=2
-        self.avatar_path=f"img/{self.name}/avatar.png"
-        self.state="idle"
+        self.stimpacks = 2
+        self.avatar_path = f"img/{self.name}/avatar.png"
+        self.state = "idle"
+
+    def set_user(self, user):
+        self.user_id = user.id
 
     def __repr__(self):
         return f"{self.name} have {self.hp} of {self.max_hp} hp"
 
     def json(self):
-        hero={"S":self.strength,
-              "P":self.perception,
-              "E":self.endurance,
-              #TODO3
-              "hp":self.hp,
-              "max_hp":self.max_hp}
+        hero = {"S": self.strength,
+                "P": self.perception,
+                "E": self.endurance,
+                # TODO3
+                "hp": self.hp,
+                "max_hp": self.max_hp}
 
         return hero
-    #TODO2
-    def is_attack_succesfull(self,obj):
-        r=random.randint(0,1)
-        if r==1:
+
+    # TODO2
+    def is_attack_succesfull(self, obj):
+        r = random.randint(0, 1)
+        if r == 1:
             return True
         else:
             return False
+
 
 class EnemyModel(db.Model):
     __tablename__ = "enemies"
@@ -78,10 +85,10 @@ class EnemyModel(db.Model):
     max_hp = db.Column(db.Integer, nullable=False)
     hp = db.Column(db.Integer, nullable=False)
     money = db.Column(db.Integer, nullable=False)
-    avatar_path=db.Column(db.String(50))
+    avatar_path = db.Column(db.String(50))
 
     def __init__(self, name):
-        self.name=name
+        self.name = name
         self.strength = random.randint(1, 2)
         self.perception = random.randint(1, 2)
         self.endurance = random.randint(1, 2)
@@ -92,13 +99,15 @@ class EnemyModel(db.Model):
         self.max_hp = self.endurance * 10 + 20
         self.hp = self.max_hp
         self.money = random.randint(10, 20)
-        self.avatar_path=f"img/{self.name}/avatar.png"
+        self.avatar_path = f"img/{self.name}/avatar.png"
 
     def __repr__(self):
         return f"{self.name} have {self.hp} of {self.max_hp} hp"
-    #TODO1
+
+    # TODO1
     def json(self):
         pass
+
 
 class UserModels(db.Model, UserMixin):
     __tablename__ = "user"
@@ -107,18 +116,17 @@ class UserModels(db.Model, UserMixin):
     login = db.Column(db.String(20), unique=True)
     mail = db.Column(db.String(256), unique=True)
     password = db.Column(db.String(50))
-    personage_id = db.Column(db.Integer, primary_key=True)
+    personage = db.relationship('PersonageModel', backref='user', uselist=False)
 
-    def __init__(self,username,email, password):
+    def __init__(self, username, email, password):
         self.login = username
         self.mail = email
         self.password = generate_password_hash(password)
-        self.personage_id = personage_id
 
     def __repr__(self):
         return f"""login: {self.login}
                     mail: {self.mail}
                     mail: {self.password} """
 
-    def check_password(self,password):
-        return check_password_hash(self.password_hash,password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
